743614f095925277e821172dd383a27be7fa3cab
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2020-08-26 21:15:03 +0200

    Amend travis config to only build master and develop (#7)

---
+++ tests/database/migrations/2020_08_25_000000_create_eloquent_models_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEloquentModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eloquent_models', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eloquent_models');
+    }
+}
